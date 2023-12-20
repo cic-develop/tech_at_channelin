9eb592d99edb7c652832bd803a3088e57d8ca6de
jutalik <team.gutalik@gmail.com>
2023-12-20 16:22:40 +0900

    write web3/klaytn libs

---
+++ src/1_web3klaytn_feeDelegate/index.js
@@ -0,0 +1,47 @@
+import ethers from "ethers";
+import { Wallet, TxType } from "@klaytn/ethers-ext";
+import { TECH_TOKEN_ABI, TECH_TOKEN_ADDRESS, EN_NODE } from "../constants.js";
+
+const provider = new ethers.providers.JsonRpcProvider(EN_NODE);
+
+const mintToken = async (to, amount) => {
+  // generate random wallet instance
+  const createWallet = new ethers.Wallet.createRandom().connect(provider);
+
+  // ethers/klaytn extension wallet instance
+  const caller = new Wallet(createWallet.privateKey, provider);
+  // ethers/klaytn extension wallet instance
+  const feePayer = new Wallet("0x{private key}", provider);
+
+  const techTokenInstance = new ethers.Contract(
+    TECH_TOKEN_ADDRESS,
+    TECH_TOKEN_ABI,
+    provider
+  );
+
+  const param = techTokenInstance.interface.encodeFunctionData("mint", [
+    to,
+    amount,
+  ]);
+
+  let tx = {
+    type: TxType.FeeDelegatedSmartContractExecution,
+    from: caller.address,
+    to: TECH_TOKEN_ADDRESS,
+    value: 0,
+    input: param,
+  };
+
+  tx = await caller.populateTransaction(tx);
+
+  const senderTxHashRLP = await caller.signTransaction(tx);
+  const sentTx = await feePayer.sendTransactionAsFeePayer(senderTxHashRLP);
+  const receipt = await sentTx.wait();
+
+  console.log("receipt=====\n", receipt);
+};
+
+//
+//
+// input your address and amount
+mintToken("0x Address ", "mint amount");
---
+++ src/1_web3klaytn_feeDelegate/readme.md
@@ -0,0 +1,3 @@
+# medium Link
+
+https://channelin-tech.medium.com/tech-web3klaytn-overview-636c9b0f980d
